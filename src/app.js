ad3e4778b171eaf313529870b1ea1afa888eedbe
Vladyslav Zakharov <vladyslavzakharovdk@gmail.com>
2024-05-06 23:25:44 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 const fs = require('fs');
+const path = require('path');
 
-const moveToFile = process.argv[3];
+let moveToFile = process.argv[3];
 const file = process.argv[2];
 
 try {
+  const isDirectory =
+    fs.existsSync(moveToFile) && fs.statSync(moveToFile).isDirectory();
+
+  if (isDirectory) {
+    const newFile = path.basename(file);
+
+    moveToFile = path.join(moveToFile, newFile);
+  }
+
   fs.renameSync(file, moveToFile);
 } catch (err) {
   global.console.error(err);
