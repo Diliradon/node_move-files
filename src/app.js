523c36037cd5bd2244f0fd0501843235405db492
Vladyslav Zakharov <vladyslavzakharovdk@gmail.com>
2024-05-06 21:08:18 +0300

    test

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-// write code here
+const fs = require('fs');
+
+const file = process.argv[2];
+const moveToFile = process.argv[3];
+
+try {
+  fs.renameSync(file, moveToFile);
+} catch (err) {
+  global.console.error(err);
+}
